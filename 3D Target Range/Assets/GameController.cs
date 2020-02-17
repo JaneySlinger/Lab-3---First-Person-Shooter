b694fef23cd23ed7b0723c0b0ac3354191f18b5e
Janey Slinger <janeyslinger@gmail.com>
2020-02-17 17:55:26 +0000

    Added UI with countdown and win message

--- 3D Target Range/Assets/GameController.cs
+++ 3D Target Range/Assets/GameController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    float timeLeft = 30.0f;
+    public float timeLeft = 20.0f;
+    public Text TimeText;
+    public Text WinText;
+    bool isGameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +18,31 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime;
+        if(!isGameOver){
+            timeLeft -= Time.deltaTime;
+        }
+
+        //format to string with no decimal places
+        TimeText.text = timeLeft.ToString("0");
         if(timeLeft < 0 ){
             Debug.Log("Game Over");
+            WinText.text = "Game Over";
+            TimeText.text = "0";
+            isGameOver = true;
         }
+
+
     }
 
     public void TargetDestroyed(){
         if(GameObject.FindObjectsOfType<Destroyable>().Length == 0){
             Debug.Log("You win!");
+            WinText.text = "You Win!";
+            isGameOver = true;
         }
+        if(!isGameOver){
+            timeLeft += 10;
+        }
+
     }
 }
